ec60af49e9a2fe16dea3ae48a2ba19da9cf5265c
stratoverus <keitheberhard93@gmail.com>
2025-03-11 00:20:42 -0700

    fixed wayfinding, added buttons

--- scripts/course.js
+++ scripts/course.js
@@ -119,3 +119,16 @@ function createClassCard(filteredCourses) {
         document.querySelector(".courses").appendChild(card);
     });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".filter button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+
+            buttons.forEach(btn => btn.classList.remove("active"));
+
+            this.classList.add("active");
+        });
+    });
+});
--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,3 +4,17 @@ hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPage = window.location.pathname.split("/").pop();
+
+    const pageMap = {
+        "index.html": "home",
+        "chamber-placeholder.html": "chamber",
+        "github-placeholder.html": "github",
+    };
+
+    if (pageMap[currentPage]) {
+        document.getElementById(pageMap[currentPage]).classList.add("active");
+    }
+});
